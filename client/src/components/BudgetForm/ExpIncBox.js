305951fe646e9b2544d7ec654cd14b909e135c40
Anne-Laure <a.creve.alc@gmail.com>
2018-10-20 12:02:13 -0700

    created folders and files

---
+++ client/src/components/BudgetForm/ExpIncBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const ExpIncBox = props => (
+<div className="form-check">
+  <input className="form-check-input" type="checkbox" value="" id="defaultCheck1">
+  <label className="form-check-label" for="defaultCheck1" {...props} >
+  
+  </label>
+</div>
+
+
+);
+
