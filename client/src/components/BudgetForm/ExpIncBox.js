7150a6831465de64b133250d10a57e0ad9e83f87
jeffrey.chee@yahoo.com <jeffrey.chee@yahoo.com>
2018-10-22 11:32:47 -0700

    Navbar

--- client/src/components/BudgetForm/ExpIncBox.js
+++ client/src/components/BudgetForm/ExpIncBox.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-export const ExpIncBox = props => (
+export const ExpIncBox = props => {
+  return (
 <div className="form-check">
-  <input className="form-check-input" type="checkbox" value="" id="defaultCheck1">
+  <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
   <label className="form-check-label" for="defaultCheck1" {...props} >
   
   </label>
 </div>
 
-
-);
+  );
+};
 
