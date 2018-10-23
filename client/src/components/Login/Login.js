1bf78925c15c7cc13a6cf38f6d962ee6c32d0842
Tarek ElHajjaoui <tarekel96@gmail.com>
2018-10-22 20:41:52 -0700

    added home, auth, login components

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+    this.change = this.change.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  submit(e) {
+    e.preventDefault();
+    axios
+      .post("/getToken", {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(res => localStorage.setItem("cool-jwt", res.data));
+  }
+  // defined here to be used later in onChange() method
+  change(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={e => this.submit(e)}>
+          <label>email:</label>
+          <input
+            type="text"
+            name="email"
+            onChange={e => this.change(e)}
+            value={this.state.email}
+          />
+          <label>password:</label>
+          <input
+            type="password"
+            name="password"
+            onChange={e => this.change(e)}
+            value={this.state.password}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
